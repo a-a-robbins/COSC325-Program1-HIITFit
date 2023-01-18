4ef0499213559451bace644dec3fec8a85444c90
Ashley Robbins <robbinsa1@central.edu>
2023-01-18 15:59:30 -0600

    updated tabbing

--- HIITFit/Views/ContentView.swift
+++ HIITFit/Views/ContentView.swift
@@ -5,22 +5,16 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-      TabView {
-          WelcomeView()
-              .tabItem {Text("Welcome")
-              }
-              .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-          
-          ForEach(0..<4) { index in ExerciseView(index: index)
-                  .tabItem {Text("Exercise")
-                  }
-                  .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-              
-          }
-//          Text("Exercise 2")
+    TabView {
+      WelcomeView()
+      ForEach(0 ..< 4) { index in
+        ExerciseView(index: index)
       }
+    }
+    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
   }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
