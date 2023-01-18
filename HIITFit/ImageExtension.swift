c5306d74d02f68a3a130cd2b7381f1c12637550e
Ashley Robbins <robbinsa1@central.edu>
2023-01-18 13:23:06 -0600

    Initial Commit

---
+++ HIITFit/ImageExtension.swift
@@ -0,0 +1,18 @@
+// based off code from SwiftUI Apprentice
+
+// modified by: a-a-robbins
+
+import SwiftUI
+
+extension Image {
+  /// Resize an image with fill aspect ratio and specified frame dimensions.
+  ///   - parameters:
+  ///     - width: Frame width.
+  ///     - height: Frame height.
+  func resizedToFill(width: CGFloat, height: CGFloat) -> some View {
+    self
+      .resizable()
+      .aspectRatio(contentMode: .fill)
+      .frame(width: width, height: height)
+  }
+}
