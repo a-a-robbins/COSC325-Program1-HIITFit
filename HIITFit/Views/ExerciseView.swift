43d60c497edde2e7c0cceddcda7d5ec643feddd2
Ashley Robbins <robbinsa1@central.edu>
2023-01-20 13:10:27 -0600

    add headers and symbols

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -8,9 +8,28 @@ struct ExerciseView: View {
     let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let index: Int
     var body: some View {
-        Text(exerciseNames[index])
+        VStack {
+            VStack {
+                Text(exerciseNames[index])
+                    .font(.largeTitle)
+            }
+            Text("Video player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+            
+            HStack {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
+            }
+            .font(.title2)
+        }
     }
 }
+
 
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
