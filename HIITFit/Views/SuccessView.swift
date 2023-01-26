1eaaa35bcdfd9b0e79df5984b5ce7b715c4c8656
Ashley Robbins <robbinsa1@central.edu>
2023-01-26 16:48:07 -0600

    add enumeration for Exercises --- creates an array structure and feeds into Views

--- HIITFit/Views/SuccessView.swift
+++ HIITFit/Views/SuccessView.swift
@@ -2,15 +2,31 @@
 import SwiftUI
 
 struct SuccessView: View {
+    let message = """
+      Good job completing all four exercises!
+      Remember tomorrow's another day.
+      So eat well and get some rest.
+      
+      """
     var body: some View {
-        VStack {
-            Image(systemName: "hand.raised.fill")
-                .resizedToFill(width: 75, height: 75)
-                .foregroundColor(.purple)
-            Text("High Five!")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .padding(.top)
+        ZStack (alignment: .topTrailing) {
+            VStack {
+                Image(systemName: "hand.raised.fill")
+                    .resizedToFill(width: 75, height: 75)
+                    .foregroundColor(.purple)
+                Text("High Five!")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .padding(.top)
+                Text(message)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+            }
+            Button(action: {}) {
+                Image(systemName: "xmark.circle")
+            }
+            .font(.title)
+            .padding(.trailing)
         }
     }
 }
