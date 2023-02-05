d652d8186438336f12c54198fdb4f62cf225b9ca
Ashley Robbins <robbinsa1@central.edu>
2023-02-05 17:26:32 -0600

    add error checking

--- HIITFit/Views/ContentView.swift
+++ HIITFit/Views/ContentView.swift
@@ -5,20 +5,31 @@ import SwiftUI
 
 struct ContentView: View {
     @SceneStorage("selectedTab") private var selectedTab = 9
-  var body: some View {
-    TabView (selection: $selectedTab) {
-        WelcomeView(selectedTab: $selectedTab)
-            .tag(9)
-        ForEach(0 ..< Exercise.exercises.count, id:\.self) { index in
-            ExerciseView(selectedTab: $selectedTab, index: index)
-                .tag(index)
-      }
+    //@Binding var showAlert: Bool
+    
+    var body: some View {
+        TabView (selection: $selectedTab) {
+            WelcomeView(selectedTab: $selectedTab)
+                .tag(9)
+            ForEach(0 ..< Exercise.exercises.count, id:\.self) { index in
+                ExerciseView(selectedTab: $selectedTab, index: index)
+                    .tag(index)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        //HELP -- FIXME
+//        .alert(isPresented: $showAlert) {
+//            Alert(
+//                title: Text("History"),
+//                message: Text (
+//                """
+//                Unfortunately we can't load your past history.
+//                Email support:
+//                    support@xyz.com
+//                """))
+//        }
     }
-    .environmentObject(HistoryStore())
-    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-  }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
