d966ed2cf9832ae35a9543e1db402190c5cfa416
Ashley Robbins <robbinsa1@central.edu>
2023-02-11 17:04:15 -0600

    c.10: added gradient background with reusable container

--- HIITFit/Views/ContainerView.swift
+++ HIITFit/Views/ContainerView.swift
@@ -2,14 +2,39 @@
 
 import SwiftUI
 
-struct ContainerView: View {
+struct ContainerView<Content: View>: View {
+    var content: Content
+    
+    init(@ViewBuilder content: () -> Content) {
+            self.content = content()
+        }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .foregroundColor(Color("background"))
+            VStack {
+                Spacer()
+                Rectangle()
+                    .frame(height: 25)
+                    .foregroundColor(Color("background"))
+            }
+            content
+        }
     }
 }
 
 struct ContainerView_Previews: PreviewProvider {
     static var previews: some View {
-        ContainerView()
+        ContainerView {
+            VStack {
+                RaisedButton(buttonText: "Hello World") {}
+                    .padding(50)
+                Button("Tap me!") {}
+                    .buttonStyle(EmbossedButtonStyle(buttonShape: .round))
+            }
+        }
+        .padding(50)
+        .previewLayout(.sizeThatFits)
     }
 }
